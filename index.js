2f65c0e5d25ecbbfb4641da36c871aab22f76162
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-09-01 21:10:16 -0400

    updated all javascript functions for application

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use this application?'
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines?'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How do you run tests for this application?'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:'
+    }
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { 
+     fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('README.md file created successfully!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        const markdown = generateMarkdown(responses);
+        writeToFile('README.md' , markdown);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'Apache 2.0':
+      return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'GPL 3.0':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'BSD 3-Clause':
+      return '![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    default:
+      return ''; 
+  }
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0)';
+    case 'GPL 3.0':
+      return '[GPL v3 License](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'BSD 3-Clause':
+      return '[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)';
+    default:
+      return ''; 
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+      return `## License\nThis project is licensed under the MIT License.\n${renderLicenseLink(license)}`;
+    case 'Apache 2.0':
+      return `## License\nThis project is licensed under the Apache 2.0 License.\n${renderLicenseLink(license)}`;
+    case 'GPL 3.0':
+      return `## License\nThis project is licensed under the GPL v3 License.\n${renderLicenseLink(license)}`;
+    case 'BSD 3-Clause':
+      return `## License\nThis project is licensed under the BSD 3-Clause License.\n${renderLicenseLink(license)}`;
+    default:
+      return ''; 
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing 
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+
+For any questions, please contact me via Github : [${data.github}](https://github.com/${data.github})
+Or by email: [${data.email}](mailto:${data.email})
 
 `;
 }
